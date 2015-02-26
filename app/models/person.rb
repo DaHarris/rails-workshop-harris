d504b3854b552dbcac4a6bbe8378ce75600c7ae9
Daniel Harris <bobth3bum@gmail.com>
2015-02-26 13:44:16 -0700

    add validations for last name and xor for first_name and title

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
+  validates :last_name, presence: true
 
-
+  validates :first_name, presence: true, if: ->(instance) { instance.title.blank? }
+  validates :title, presence: true, if: ->(instance) { instance.first_name.blank? }
 
 end
